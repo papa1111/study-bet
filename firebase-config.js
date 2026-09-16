923ec8e7fbd8e10fe8a47fc398e228a7001e79d1
papa1111 <49470229+papa1111@users.noreply.github.com>
2026-09-16 19:00:03 +0900

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAGmRT4K_y2l4qP7A2sG8PWyGUNjnyapt8",
+  authDomain: "study-bet-dd58b.firebaseapp.com",
+  projectId: "study-bet-dd58b",
+  storageBucket: "study-bet-dd58b.firebasestorage.app",
+  messagingSenderId: "48290934455",
+  appId: "1:48290934455:web:c3d56fae53812a45b4a732"
+};
